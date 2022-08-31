eb4dcc9b776e1bad2867e670f305941c3d436d54
cloud-bash <79466037+cloud-bash@users.noreply.github.com>
2022-08-31 17:47:19 -0400

    Create aws-cli-commands.sh

---
+++ aws-cli-commands.sh
@@ -0,0 +1,8 @@
+curl 169.254.169.254/latest/meta-data/
+# The above returns a list of api endpoints
+# Append any of the endpoints to the url to read the values
+# It's just like navigating directories in Linux
+
+curl 169.254.169.254/latest/meta-data/mac
+# The above returns the mac address of your instance
+# This is one great example of a REST API
